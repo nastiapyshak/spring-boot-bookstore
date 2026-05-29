32b5e1bde3821e934f0b261784d65749ef31ad3b
nastiapyshak <nastapisak618@gmail.com>
2026-05-29 06:06:33 +0200

    Initial project setup

---
+++ src/main/java/mate/academy/springbootbookstore/SpringBootBookstoreApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/springbootbookstore/SpringBootBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
