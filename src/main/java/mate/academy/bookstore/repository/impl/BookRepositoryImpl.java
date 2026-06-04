5edf8a8c0ef285611b331d4ca899e8ceac802f8e
Karolina Pyszak <96234596+nastiapyshak@users.noreply.github.com>
2026-06-04 06:48:54 +0200

    Implement Book entity and project configuration. (#1)
    * Implement Book entity and project configuration.
    
    * Add DataProcessingException, used try with resources

---
+++ src/main/java/mate/academy/bookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,51 @@
+package mate.academy.bookstore.repository.impl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.bookstore.exception.DataProcessingException;
+import mate.academy.bookstore.model.Book;
+import mate.academy.bookstore.repository.BookRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Override
+    public Book save(Book book) {
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can not persist Book entity", e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager
+                    .createQuery("FROM Book", Book.class)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not get list of Books form DB", e);
+        }
+    }
+}
