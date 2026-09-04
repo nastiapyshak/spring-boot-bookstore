3946634c3ae6db34a1432ea011bf0a8164080ba8
nastiapyshak <nastapisak618@gmail.com>
2026-09-04 04:07:20 +0200

    Add Pagination, Sorting, and Swagger to the existing controllers.

--- src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
@@ -1,6 +1,5 @@
 package mate.academy.bookstore.service.impl;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.CreateBookRequestDto;
@@ -9,6 +8,8 @@ import mate.academy.bookstore.mapper.BookMapper;
 import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.BookRepository;
 import mate.academy.bookstore.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,10 +34,9 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
-                .map(bookMapper::toDto)
-                .toList();
+    public Page<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable)
+                .map(bookMapper::toDto);
     }
 
     @Override
